5fea606e553f437fc7999f5648ae10c6b6cd005a
Wout van den Berg <wout_van_den_berg@hotmail.com>
2023-10-30 10:14:26 +0100

    added analysis, example website and cookie headers

--- collectors/RequestCollector.js
+++ collectors/RequestCollector.js
@@ -7,7 +7,7 @@ const URL = require('url').URL;
 const crypto = require('crypto');
 const {Buffer} = require('buffer');
 
-const DEFAULT_SAVE_HEADERS = ['etag', 'set-cookie', 'cache-control', 'expires', 'pragma', 'p3p', 'timing-allow-origin', 'access-control-allow-origin', 'accept-ch'];
+const DEFAULT_SAVE_HEADERS = ['etag', 'cookie', 'set-cookie', 'cache-control', 'expires', 'pragma', 'p3p', 'timing-allow-origin', 'access-control-allow-origin', 'accept-ch'];
 
 class RequestCollector extends BaseCollector {
 
